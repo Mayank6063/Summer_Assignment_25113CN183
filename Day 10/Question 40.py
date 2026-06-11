27ce1e2ed2b1823dca7dbe7830b07a89bd2e9570
Mayank6063 <sharmamayank1187@gmail.com>
2026-06-11 16:45:06 +0530

    Add files via upload

---
+++ Day 10/Question 40.py
@@ -0,0 +1,12 @@
+n = 5
+
+for i in range(n):
+    print(" " * (n - i - 1), end="")
+
+    for j in range(i + 1):
+        print(chr(65 + j), end="")
+
+    for j in range(i - 1, -1, -1):
+        print(chr(65 + j), end="")
+
+    print()
